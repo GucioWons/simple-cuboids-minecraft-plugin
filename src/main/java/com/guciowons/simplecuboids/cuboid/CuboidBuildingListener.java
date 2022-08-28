fe7554c4e61dd3586a12cc281092198c5129f7fb
GucioWons <wgutek1@wp.pl>
2022-08-28 17:15:02 +0200

    Checking if broken block is on cuboid

--- src/main/java/com/guciowons/simplecuboids/cuboid/CuboidBuildingListener.java
+++ src/main/java/com/guciowons/simplecuboids/cuboid/CuboidBuildingListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class CuboidBuildingListener implements Listener {
@@ -12,6 +13,14 @@ public class CuboidBuildingListener implements Listener {
         Block placedBlock = e.getBlock();
         Location placedBlockLocation = placedBlock.getLocation();
         CuboidRepository.isBlockAtCuboid(placedBlockLocation.getBlockX(), placedBlockLocation.getBlockZ())
-                .ifPresent(cuboid -> e.getPlayer().sendMessage("Cuboid"));
+                .ifPresent(cuboid -> e.getPlayer().sendMessage("Block placed"));
+    }
+
+    @EventHandler
+    public void onBlockBreakAtCuboid(BlockBreakEvent e){
+        Block brokenBlock = e.getBlock();
+        Location brokenBlockLocation = brokenBlock.getLocation();
+        CuboidRepository.isBlockAtCuboid(brokenBlockLocation.getBlockX(), brokenBlockLocation.getBlockZ())
+                .ifPresent(cuboid -> e.getPlayer().sendMessage("Block broken"));
     }
 }
